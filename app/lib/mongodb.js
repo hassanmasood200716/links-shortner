ad8fd9eb0281a1d4f05b1702f5590c336ed0762d
hassanmasood200716 <hassanmasood9876@gmail.com>
2025-02-08 15:40:21 +0500

    again

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -4,9 +4,7 @@
 import { MongoClient } from 'mongodb'
 
 const uri = process.env.MONGODB_URI
-const options = { 
-  useNewUrlParser: true,
-}
+
 
 let client
 let clientPromise
@@ -17,12 +15,12 @@ if (!process.env.MONGODB_URI) {
 
 if (process.env.NODE_ENV === 'development') { 
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
+    client = new MongoClient(uri)
     global._mongoClientPromise = client.connect()
   }
   clientPromise = global._mongoClientPromise
 } else {
-  client = new MongoClient(uri, options)
+  client = new MongoClient(uri)
   clientPromise = client.connect()
 }
 
