5fa969164cfe21e262baecbbfcb7b4cf9e515f04
hassanmasood200716 <hassanmasood9876@gmail.com>
2025-02-08 15:34:20 +0500

    again

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -15,7 +15,7 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Add Mongo URI to .env.local')
 }
 
-if (process.env.NODE_ENV === 'production') { 
+if (process.env.NODE_ENV === 'development') { 
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options)
     global._mongoClientPromise = client.connect()
